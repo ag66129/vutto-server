cd32f61ba1d6acd50d064d52935d5076f0151612
Ashish Gupta <ag66129@gmail.com>
2025-09-03 18:40:46 +0530

    update index

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/bikes', bikeRoutes);
 
-sequelize.sync().then(() => {
-    console.log("✅ Database synced successfully");
-    // app.listen(process.env.PORT, () => {
-    //     console.log(`Server running on port ${process.env.PORT}`);
-    // });
-});
+// sequelize.sync().then(() => {
+//     console.log("✅ Database synced successfully");
+//     app.listen(process.env.PORT, () => {
+//         console.log(`Server running on port ${process.env.PORT}`);
+//     });
+// });
+
+sequelize.authenticate()
+  .then(() => console.log("✅ Database connected"))
+  .catch(err => console.error("❌ Database connection failed:", err));
+
 
 module.exports = app;
