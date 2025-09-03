9edcb621a948617ded5975f7f7c31a6c3e71b089
Ashish Gupta <ag66129@gmail.com>
2025-09-03 18:23:52 +0530

    update sequlize

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,10 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL)
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres", // or mysql
+    dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
+})
 // new Sequelize({
 //     username: process.env.DB_USER,
 //     password: process.env.DB_PASS,
