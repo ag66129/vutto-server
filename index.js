41de41334af1b826292ddf6d51c9ddbd7954616f
Ashish Gupta <ag66129@gmail.com>
2025-09-03 18:09:02 +0530

    change indes file

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use('/auth', authRoutes);
 app.use('/bikes', bikeRoutes);
 
 sequelize.sync().then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server running on port ${process.env.PORT}`);
-    });
+    console.log("✅ Database synced successfully");
+    // app.listen(process.env.PORT, () => {
+    //     console.log(`Server running on port ${process.env.PORT}`);
+    // });
 });
